6d1aa9e728c7c94506e5b0b3a4d35186e391aec9
Douglas Holanda <douglasholanda3195@gmail.com>
2024-02-11 17:01:31 -0300

    My first commit for this project

---
+++ src/application/Program.java
@@ -0,0 +1,9 @@
+package application;
+
+import static utils.Utility.println;
+
+public class Program {
+    public static void main(String[] args) {
+        println("Welcome to bank system management.\n");
+    }
+}
---
+++ src/utils/Utility.java
@@ -0,0 +1,9 @@
+package utils;
+
+public class Utility {
+    public static void println(String message){
+        System.out.println("*************************************\n");
+        System.out.println(message);
+        System.out.println("*************************************\n");
+    }
+}
