bdbadccf134c799d048c154830e826ce6dae2009
Douglas Holanda <douglasholanda3195@gmail.com>
2024-02-11 17:10:18 -0300

    function first interaction created

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,33 @@
 package application;
 
 import static utils.Utility.println;
+import static utils.Utility.sc;
 
 public class Program {
     public static void main(String[] args) {
         println("Welcome to bank system management.\n");
+        doFirstInteraction();
+    }
+    public static void doFirstInteraction(){
+        println("Who are you?");
+        System.out.println("    M/m - Bank manager\n");
+        System.out.println("    B/b - Bank clerk\n");
+        System.out.println("    C/c - Customer\n");
+        String option = sc.nextLine();
+
+        switch(option.toLowerCase()){
+            case "m" -> {
+
+            }
+            case "b" -> {
+
+            }
+            case "c" -> {
+
+            }
+            default -> {
+                println("Sorry, however this option´s no existent.\n");
+            }
+        }
     }
 }
--- src/utils/Utility.java
+++ src/utils/Utility.java
@@ -1,6 +1,10 @@
 package utils;
 
+import java.util.Scanner;
+
 public class Utility {
+
+    public static Scanner sc = new Scanner(System.in);
     public static void println(String message){
         System.out.println("*************************************\n");
         System.out.println(message);
